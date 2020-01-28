f100cf6d66b6e363ed3c7439e876e91a07a14799
Sebastian Doe <sebastiandoe5@gmail.com>
2020-01-28 13:05:30 +0000

    Add new loading animation

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function pageLoad() {
-        
+    getCountry();
+    fadeOutLoad();
+}
+
+function fadeOutLoad() {
+    setTimeout(
+    function() {
+        $(".loadDiv").fadeOut();
+    }, 1000);
 }
 
 function getCountry() {
-    $.get("https://ipinfo.io", function(response) {
-    console.log(response.city, response.country);
-}, "jsonp");
+    jQuery(document).ready(function($) {
+    jQuery.getScript('http://www.geoplugin.net/javascript.gp', function() 
+    {
+    var country = geoplugin_countryName();
+    var zone = geoplugin_region();
+    var district = geoplugin_city();
+    console.log("Your location is: " + country + ", " + zone + ", " + district);
+    });
+    });
 }
 
 function createNumber() {
