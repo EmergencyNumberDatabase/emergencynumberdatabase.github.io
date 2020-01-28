ed8a3a936d2d7549c1472c283296fbc56d299faa
Sebastian Doe <sebastiandoe5@gmail.com>
2020-01-28 17:58:10 +0000

    Try to add service workers

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function createNumber() {
 function createNew1() {
 
 }
+
+function createNew2() {
+
+}
+
+function createNew3() {
+
+}
+
+function createNew4() {
+
+}
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(function(registration) {
+            console.log('Service Worker Registered');
+      });
+    navigator.serviceWorker.ready.then(function(registration) {
+       console.log('Service Worker Ready');
+    });
+}
---
+++ sw.js
@@ -0,0 +1,36 @@
+const version = "1.0.1";
+const cacheName = `endb`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/index.html`,
+        `/script.js`,
+        `/style.css`,
+        `/script.js`,
+        `/lib/jquery-3.4.1.js`,
+        `/README.md`,
+        `/site.webmanifest`,
+        `/favicon.ico`,
+        `/html_code.html`,
+        `/src/animation/loading.svg`,
+        `/src/icon/logo/1500x1500.png`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
